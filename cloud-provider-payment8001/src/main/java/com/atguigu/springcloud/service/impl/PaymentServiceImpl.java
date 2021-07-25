0cbbb88c1bef98012e93a31e74b11e6020effc9c
zhu <zhu@qq.com>
2021-07-25 17:20:59 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.dao.paymentDao;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Resource
+    private com.atguigu.springcloud.dao.paymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
