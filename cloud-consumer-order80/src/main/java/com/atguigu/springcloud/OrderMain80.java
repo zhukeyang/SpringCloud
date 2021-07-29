68e6a8b4f947073b26ff966f2900ef6cf90f1071
zhu <zhu@qq.com>
2021-07-29 16:43:25 +0800

    day02 two commit

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -2,7 +2,9 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderMain80 {
     public static void main(String[] args) {
