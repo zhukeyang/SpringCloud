8829ec219b3222ee1d210fa2010a2d4c02501d82
zhu <zhu@qq.com>
2021-08-07 17:56:58 +0800

    day09 third commit

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/PaymentHystrixMain8001.java
@@ -1,10 +1,16 @@
 package com.atguigu.springcloud;
 
+import com.netflix.hystrix.HystrixMetrics;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.ServletRegistration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableCircuitBreaker
@@ -12,5 +18,16 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet()
+    {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
