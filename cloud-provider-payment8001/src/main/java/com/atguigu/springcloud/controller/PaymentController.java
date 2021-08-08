389cdd09449fce77ce9b10bf0e9161e41764a69a
zhu <zhu@qq.com>
2021-08-08 11:54:01 +0800

    day10 first commit

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -45,7 +45,7 @@ public class PaymentController {
     public CommonResult getPaymentById(@PathVariable("id") Long id)
     {
         Payment payment = paymentService.getPaymentById(id);
-        log.info("******插入结果:"+payment);
+        log.info("******查询结果:"+payment);
         if(payment!=null)
         {
             return new CommonResult(200,"查询成功,server.port:"+serverPort,payment);
